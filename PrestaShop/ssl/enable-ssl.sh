5e36c059b3926d0fe20bb7266baf953f28f0ab45
Marcel Zielinski <marziel2001@gmail.com>
2023-12-01 03:09:07 +0100

    finished auto ssl script

---
+++ PrestaShop/ssl/enable-ssl.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+cd /var/www/html
+mkdir ssl
+chmod 777 ssl
+cd ssl
+
+expect -c '
+spawn openssl genrsa -des3 -out server.key 2048
+expect "Enter pass phrase for server.key:"
+send "1111\n"
+expect "Verifying - Enter pass phrase for server.key:"
+send "1111\n"
+expect eof
+'
+echo "koniec"
+
+expect -c '
+spawn openssl req -new -key server.key -out server.csr
+match_max 100000
+expect -exact "Enter pass phrase for server.key:"
+send -- "1111\r"
+expect -exact "\r
+You are about to be asked to enter information that will be incorporated\r
+into your certificate request.\r
+What you are about to enter is what is called a Distinguished Name or a DN.\r
+There are quite a few fields but you can leave some blank\r
+For some fields there will be a default value,\r
+If you enter '.', the field will be left blank.\r
+-----\r
+Country Name (2 letter code) \[AU\]:"
+send -- "\r"
+expect -exact "\r
+State or Province Name (full name) \[Some-State\]:"
+send -- "\r"
+expect -exact "\r
+Locality Name (eg, city) \[\]:"
+send -- "\r"
+expect -exact "\r
+Organization Name (eg, company) \[Internet Widgits Pty Ltd\]:"
+send -- "\r"
+expect -exact "\r
+Organizational Unit Name (eg, section) \[\]:"
+send -- "\r"
+expect -exact "\r
+Common Name (e.g. server FQDN or YOUR name) \[\]:"
+send -- "localhost\r"
+expect -exact "localhost\r
+Email Address \[\]:"
+send -- "\r"
+expect -exact "\r
+\r
+Please enter the following 'extra' attributes\r
+to be sent with your certificate request\r
+A challenge password \[\]:"
+send -- "\r"
+expect -exact "\r
+An optional company name \[\]:"
+send -- "\r"
+expect eof
+'
+
+cp server.key server.key.org;
+expect -c '
+spawn openssl rsa -in server.key.org -out server.key
+expect "Enter pass phrase for server.key.org:"
+send "1111\n"
+expect eof
+'
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+
+cp server.crt server.csr server.key server.key.org /etc/apache2/ssl/
+cp -r 000-default.conf /etc/apache2/sites-enabled
+
+a2enmod ssl;service apache2 restart
