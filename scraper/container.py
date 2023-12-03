5ca7e71e75a1c30257829d5c96a5baf08e0e3644
Paweł Staszczuk <41273017+staszczuk@users.noreply.github.com>
2023-12-03 15:00:59 +0100

    Change csv delimiter to |

--- scraper/container.py
+++ scraper/container.py
@@ -28,13 +28,13 @@ class Container:
             Container.overridden_files.append(self._file_path)
 
         with open(self._file_path, "a", buffering=1) as file:
-            writer = csv.writer(file)
+            writer = csv.writer(file, delimiter="|")
 
             for element in self._elements:
                 writer.writerow(element.get_attribute_values())
 
     def _write_attribute_names(self):
         with open(self._file_path, "w", buffering=1) as file:
-            writer = csv.writer(file)
+            writer = csv.writer(file, delimiter="|")
             names = self._elements[0].get_attribute_names()
             writer.writerow(names)
