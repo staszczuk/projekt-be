141dbd08e0ccc324199ba26a50bb38658450ab34
Paweł Staszczuk <41273017+staszczuk@users.noreply.github.com>
2023-12-03 01:19:50 +0100

    Update csv

--- scraper/element.py
+++ scraper/element.py
@@ -6,9 +6,6 @@ class Element:
         self._attributes = {"id": Element.id}
         Element.id += 1
 
-    def add_attribute(self, name, value):
-        self._attributes.update({name: value})
-
     def get_attribute(self, name):
         return self._attributes.get(name)
 
@@ -20,3 +17,6 @@ class Element:
 
     def get_link(self):
         return self._link
+
+    def set_attribute(self, name, value):
+        self._attributes.update({name: value})
