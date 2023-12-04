bab55f9a0e561c50c21bd25ff63c9d28463722d6
Marcel Zielinski <marziel2001@gmail.com>
2023-12-04 21:03:48 +0100

    fixed weights to temp 1kg

--- PrestaShop/var/cache/dev/smarty/cache/ps_linklist/displayFooter/1/3/3/14/classic/da/d3/63/dad363d340027902e5ae2944c5ffb52782d7afc3.pslinklistviewstemplatesh.php
+++ PrestaShop/var/cache/dev/smarty/cache/ps_linklist/displayFooter/1/3/3/14/classic/da/d3/63/dad363d340027902e5ae2944c5ffb52782d7afc3.pslinklistviewstemplatesh.php
@@ -1,11 +1,11 @@
 <?php
-/* Smarty version 3.1.48, created on 2023-12-04 20:35:03
+/* Smarty version 3.1.48, created on 2023-12-04 20:58:00
   from 'module:pslinklistviewstemplatesh' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.48',
-  'unifunc' => 'content_656e29e70c7338_02100055',
+  'unifunc' => 'content_656e2f480beee3_04054879',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -18,13 +18,13 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   ),
   'cache_lifetime' => 31536000,
 ),true)) {
-function content_656e29e70c7338_02100055 (Smarty_Internal_Template $_smarty_tpl) {
+function content_656e2f480beee3_04054879 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->smarty->ext->_tplFunction->registerTplFunctions($_smarty_tpl, array (
   'renderLogo' => 
   array (
-    'compiled_filepath' => '/var/www/html/var/cache/dev/smarty/compile/classiclayouts_layout_full_width_tpl/d2/2d/85/d22d851f6d7c12eb35c1c34c8e84baa801a8cea2_2.file.helpers.tpl.php',
+    'compiled_filepath' => '/var/www/html/var/cache/dev/smarty/compile/classiclayouts_layout_left_column_tpl/d2/2d/85/d22d851f6d7c12eb35c1c34c8e84baa801a8cea2_2.file.helpers.tpl.php',
     'uid' => 'd22d851f6d7c12eb35c1c34c8e84baa801a8cea2',
-    'call_name' => 'smarty_template_function_renderLogo_459713307656a45d46d0919_21298488',
+    'call_name' => 'smarty_template_function_renderLogo_1977645522656a4811d54a17_37830956',
   ),
 ));
 ?><!-- begin /var/www/html/themes/classic/modules/ps_linklist/views/templates/hook/linkblock.tpl --><div class="col-md-6 links">
